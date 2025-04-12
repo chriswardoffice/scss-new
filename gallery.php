77d821e2afb91bb5cc567434d88b3ecabce6ebcc
Christopher Ward <christopher.wardnz@gmail.com>
2025-04-12 11:12:30 +0100

    Create gallery.php

---
+++ gallery.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Gallery | Christopher Ward Construction</title>
+    <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <div class="spacer"></div>
+    <div class="container">
+        <h1>Gallery</h1>
+        <nav>
+            <a href="index.php"><span>Home</span></a>
+            <a href="gallery.php"><span>Gallery</span></a>
+            <a href="index.php#contact"><span>Contact</span></a>
+        </nav>
+        <div class="gallery">
+            <div class="gallery-item">
+                <img src="images/myphoto.jpg" alt="2001 Range Rover">
+                <p class="caption">2001 Range Rover, capable of towing up to 3500kg with appropriate trailer</p>
+            </div>
+            <div class="gallery-item">
+                <img src="images/table.jpg" alt="Dining Table and Bench">
+                <p class="caption">A lovely Dining Table and Bench made for a client. Made with locally sourced Elm, and custom-made metal legs</p>
+            </div>
+            <div class="gallery-item">
+                <div class="gallery-pair">
+                    <img src="images/cabinet.jpg" alt="Bathroom Cabinet">
+                    <img src="images/shower.jpg" alt="Bathroom Renovation">
+                </div>
+                <p class="caption">A lovely custom designed bathroom cabinet with a mirror front, and a full bathroom renovation, and install</p>
+            </div>
+        </div>
+    </div>
+    <div class="spacer"></div>
+</body>
+
+</html>
